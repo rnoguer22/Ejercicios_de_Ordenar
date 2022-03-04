e9c92c91718f4468b8f152ef723db1bd3500fc96
rnoguer22 <rnogugon@myuax.com>
2022-03-04 18:59:43 +0100

    Completamos la funcion "ordenar"

--- Insercion_dicotonomica.py
+++ Insercion_dicotonomica.py
@@ -4,5 +4,22 @@ class dicotonomia:
     #Definimos el constructor
     def __init__(self, tabla):
         self.tabla = tabla
+        self.n = min(self.tabla)
+        self.contador = 0
     
-    
+    def ordenar(self):
+        #Definimos una lista vacia que contendra los elementos de "tabla" ordenados crecientemente
+        r = []
+        if self.n == min(self.tabla):
+            r.append(self.n)
+            self.n += 1
+            self.contador += 1
+            self.ordenar()
+        else:
+            if self.n in self.tabla:
+                r.append(self.n)
+                self.n += 1
+                self.contador += 1
+                self.ordenar()
+            elif self.contador >= len(self.tabla):
+                return r
