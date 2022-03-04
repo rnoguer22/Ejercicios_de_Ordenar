7a1be65494db5c6ef1a6d20fee38a1fe3c9435ec
rnoguer22 <rnogugon@myuax.com>
2022-03-04 19:12:46 +0100

    Solucion errores

--- Insercion_dicotonomica.py
+++ Insercion_dicotonomica.py
@@ -1,25 +1,29 @@
-tabla = [4,2,6,3,8,7,5,9,1,0]
-
-class dicotonomia:
+class Dicotonomia:
     #Definimos el constructor
     def __init__(self, tabla):
         self.tabla = tabla
         self.n = min(self.tabla)
         self.contador = 0
+        #Definimos una lista vacia que contendra los elementos de "tabla" ordenados crecientemente
+        self.r = []
     
     def ordenar(self):
-        #Definimos una lista vacia que contendra los elementos de "tabla" ordenados crecientemente
-        r = []
         if self.n == min(self.tabla):
-            r.append(self.n)
+            self.r.append(self.n)
             self.n += 1
             self.contador += 1
             self.ordenar()
         else:
             if self.n in self.tabla:
-                r.append(self.n)
+                self.r.append(self.n)
                 self.n += 1
                 self.contador += 1
                 self.ordenar()
-            elif self.contador >= len(self.tabla):
-                return r
+            elif self.contador == len(self.tabla):
+                return self.r
+
+if __name__ == "__main__":
+
+    tabla = [4,2,6,3,8,7,5,9,1,0]
+    resultado = Dicotonomia(tabla)
+    print ("La tabla ordenada por dicotonomia es: {}".format(resultado.ordenar()))
