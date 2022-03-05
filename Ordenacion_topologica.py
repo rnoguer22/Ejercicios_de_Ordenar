8ac88030589f540596b46e3add59828412a48c1f
rnoguer22 <rnogugon@myuax.com>
2022-03-05 12:51:15 +0100

    Implementacion de POO para la resolucion del ejercicio

--- Ordenacion_topologica.py
+++ Ordenacion_topologica.py
@@ -1,9 +1,15 @@
 lista = [5,7,3,-4,8,12,9,2,8]
 
-for i in range(len(lista)-1):
-    for j in range(len(lista)-1):
-        if lista[j] > lista[j+1]:
-            #Intercambiamos los elementos mediante una asignacion multiple
-            lista[j], lista[j+1] = lista[j+1], lista[j]
+class Lista:
+    def __init__(self, lista):
+        self.lista = lista
+    
+    def ordneacion_burbuja(self):
+        for i in range(len(self.lista)-1):
+            for j in range(len(self.lista)-1):
+                if self.lista[j] > self.lista[j+1]:
+                    #Intercambiamos los elementos mediante una asignacion multiple
+                    self.lista[j], self.lista[j+1] = self.lista[j+1], self.lista[j]
+        return self.lista
 
 print(lista)
