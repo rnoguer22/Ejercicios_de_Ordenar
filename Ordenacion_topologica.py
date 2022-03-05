709f38dd87fd45b12433dbbb989fc16e3f6e961f
rnoguer22 <rnogugon@myuax.com>
2022-03-05 13:29:43 +0100

    Intento fallido de aplicar recursividad :(

--- Ordenacion_topologica.py
+++ Ordenacion_topologica.py
@@ -1,17 +1,31 @@
 class Lista:
-    def __init__(self, lista):
+    def __init__(self, lista, i):
         self.lista = lista
+        self.i = 0
+        self.j = 0
     
-    def ordneacion_burbuja(self):
-        for i in range(len(self.lista)-1):
-            for j in range(len(self.lista)-1):
+    def ordenacion_burbuja(self):
+
+        def recorrer_lista_2(j):
+            if j == (len(self.lista)-1):
+                recorrer_lista(self.i+1)
+            else:
                 if self.lista[j] > self.lista[j+1]:
-                    #Intercambiamos los elementos mediante una asignacion multiple
+                #Intercambiamos los elementos de la lista mediante una asignacion multiple
                     self.lista[j], self.lista[j+1] = self.lista[j+1], self.lista[j]
-        return self.lista
+                    print (self.lista)
+            recorrer_lista_2(j+1)
+
+        def recorrer_lista(i):
+            if i == len(self.lista)-1:
+                return self.lista
+            else:
+                recorrer_lista_2(self.j+i)
+        
+        recorrer_lista(self.i)
 
 if __name__ == "__main__":
     
-    vector = [5,7,3,-4,8,12,9,2,8]
-    resultado = Lista(vector)
-    print (resultado.ordneacion_burbuja())
+    vector = [5,7,3,-4,8,12,9,2,14]
+    resultado = Lista(vector, 0)
+    print (resultado.ordenacion_burbuja())
