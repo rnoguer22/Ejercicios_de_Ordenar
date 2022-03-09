e47c69af394e52d5f47749a1dc4de4df295ee98c
rnoguer22 <rnogugon@myuax.com>
2022-03-09 19:03:11 +0100

    Aplicamos POO

--- Explorar.py
+++ Explorar.py
@@ -1,40 +1,44 @@
-vector = [5,7,3,-4,8,12,9,2,8]
+class Segmento:
+    #Definimos el constructor
+    def __init__(self, tabla):
+        self.tabla = tabla
 
-#Funcion para hallar los segmentos de una tabla 
-def segmentos(tabla):
-    #En esta lista vacia se almacenaran los segmentos
-    r = []
-    for i in range(len(tabla)-1):
-        #Si el elemento de la izquierda > derecha añadimos el numero a r (definicion de segmento)
-        if tabla[i] >= tabla[i+1]:
-            r.append(tabla[i])
-            #Condicional para añadir el ultimo elemento del segmento
-            if tabla[i+1] < tabla[i+2]:
-                r.append(tabla[i+1])
-                #Repetimos el mismo proceso para hallar el segundo segmento
-                n = []
-                #Con la diferencia de que partimos del indice del ultimo elemento del segmento
-                for j in range(i+2, len(tabla)-1):
-                    if tabla[j] >= tabla[j+1]:
-                        n.append(tabla[j])
-                        if tabla[j+1] < tabla[j+2]:
-                            n.append(tabla[j+1])
-                            #Devuelve una lista con ambos segmentos
-                            return r, n
-        else:
-            pass
+    #Funcion para hallar los segmentos de una tabla 
+    def segmentos(self):
+        #En esta lista vacia se almacenaran los segmentos
+        r = []
+        for i in range(len(self.tabla)-1):
+            #Si el elemento de la izquierda > derecha añadimos el numero a r (definicion de segmento)
+            if self.tabla[i] >= self.tabla[i+1]:
+                r.append(self.tabla[i])
+                #Condicional para añadir el ultimo elemento del segmento
+                if self.tabla[i+1] < self.tabla[i+2]:
+                    r.append(self.tabla[i+1])
+                    #Repetimos el mismo proceso para hallar el segundo segmento
+                    n = []
+                    #Con la diferencia de que partimos del indice del ultimo elemento del segmento
+                    for j in range(i+2, len(self.tabla)-1):
+                        if self.tabla[j] >= self.tabla[j+1]:
+                            n.append(self.tabla[j])
+                            if self.tabla[j+1] < self.tabla[j+2]:
+                                n.append(self.tabla[j+1])
+                                #Devuelve una lista con ambos segmentos
+                                return r, n
+            else:
+                pass
 
-def explorar(segmento):
-    #Hacemos copia de seguidad del maximo del segmento, tal y como nos dice el enunciado
-    mi = segmento[0]
-    #Desplazamos los elementos a la izquierda, concatenando dos sublistas de segmento
-    segmento = segmento[1:] + segmento[:1]
-    #Devolvemos el segmento ordenado
-    return segmento
+    def explorar(self, segmento):
+        #Hacemos copia de seguidad del maximo del segmento, tal y como nos dice el enunciado
+        mi = segmento[0]
+        #Desplazamos los elementos a la izquierda, concatenando dos sublistas de segmento
+        segmento = segmento[1:] + segmento[:1]
+        #Devolvemos el segmento ordenado
+        return segmento
 
-print (segmentos(vector))
-segmento_1, segmento_2 = segmentos(vector)[0], segmentos(vector)[1]
-print(segmento_1)
-print(segmento_2)
+if __name__ == "__main__":
 
-print(explorar(segmento_1))
+    vector = [5,7,3,-4,8,12,9,2,8]
+    resultado = Segmento(vector)
+    print ("Los segmentos de {} son: {}".format(vector, resultado.segmentos()))
+    print("Tras aplicar la funcion explorar los segmentos quedan de la siguiente manera:")
+    print (resultado.explorar(resultado.segmentos()[0]), " - ", resultado.explorar(resultado.segmentos()[1]))
