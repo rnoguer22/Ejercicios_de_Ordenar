241f8f3b9b27ae4706922cd4423c16a72602d66e
rnoguer22 <rnogugon@myuax.com>
2022-03-09 18:17:31 +0100

    Solucion errores en "segmentos" Vease que esta funcion se puede optimizar en gran medida

--- Explorar.py
+++ Explorar.py
@@ -1,15 +1,20 @@
 vector = [5,7,3,-4,8,12,9,2,8]
 
 def segmentos(tabla):
-    def segmento(j):
-        r = []
-        for i in range(j, len(tabla)-1):
-            if tabla[i] >= tabla[i+1]:
-                r.append(tabla[i])
-                if tabla[i+1] < tabla[i+2]:
-                    r.append(tabla[i+1])
-                    return r
-            else:
-                pass
+    r = []
+    for i in range(len(tabla)-1):
+        if tabla[i] >= tabla[i+1]:
+            r.append(tabla[i])
+            if tabla[i+1] < tabla[i+2]:
+                r.append(tabla[i+1])
+                n = []
+                for j in range(i+2, len(tabla)-1):
+                    if tabla[j] >= tabla[j+1]:
+                        n.append(tabla[j])
+                        if tabla[j+1] < tabla[j+2]:
+                            n.append(tabla[j+1])
+                            return r, n
+        else:
+            pass
 
 print (segmentos(vector))
