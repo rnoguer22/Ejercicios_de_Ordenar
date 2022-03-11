fc5d70234cf1676bff5df904a3852c3713708da0
rnoguer22 <rnogugon@myuax.com>
2022-03-11 12:28:35 +0100

    Modificamos el codigo

--- Clases/Insercion_dicotonomica.py
+++ Clases/Insercion_dicotonomica.py
@@ -1,3 +1,4 @@
+"Apartado 1"
 class Dicotonomia_1:
     #Definimos el constructor
     def __init__(self, tabla):
@@ -8,17 +9,31 @@ class Dicotonomia_1:
         self.tabla.sort()
         return self.tabla
 
+"Apartado 2"
 class Dicotonomia_2:
     def __init__(self, tabla):
         #Heredamos el constructor de la clase Dicotonomia_1
         Dicotonomia_1.__init__(self, tabla)
-    
+        r = []
+        self.r = r
 
+    def ordenar_lista_vacia(self):
+        #Heredamos la funcion ordenar de la clase Dicotonomia_1
+        Dicotonomia_1.ordenar(self)
+        #Bucle para añadir cada elemento de la lista ordenada a la lista vacia
+        for i in self.tabla:
+            self.r.append(i)
+        return self.r
 
 if __name__ == "__main__":
 
+    #Apartado a)
     tabla = [4,2,6,3,8,7,5,9,1,0]
     print (tabla)
     #Definimos nuestra unica instancia de clase
     resultado = Dicotonomia_1(tabla)
     print ("La tabla ordenada por dicotonomia es: {}".format(resultado.ordenar()))
+
+    #Apartado b)
+    resultado_2 = Dicotonomia_2(tabla)
+    print ("Ordenando a partir de una lista vacia, la lista es: {}".format(resultado_2.ordenar_lista_vacia()))
