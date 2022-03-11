99ee7dc6d83cca2e1ba1277f23fcba91bdb6cfe3
rnoguer22 <rnogugon@myuax.com>
2022-03-11 12:14:20 +0100

    Creacion de la clase Dictonomia_2 El constructor de esta clase se hereda de la clase "Dicotonomia_1"

--- Clases/Insercion_dicotonomica.py
+++ Clases/Insercion_dicotonomica.py
@@ -1,4 +1,4 @@
-class Dicotonomia:
+class Dicotonomia_1:
     #Definimos el constructor
     def __init__(self, tabla):
         self.tabla = tabla
@@ -8,10 +8,17 @@ class Dicotonomia:
         self.tabla.sort()
         return self.tabla
 
+class Dicotonomia_2:
+    def __init__(self, tabla):
+        #Heredamos el constructor de la clase Dicotonomia_1
+        Dicotonomia_1.__init__(self, tabla)
+    
+
+
 if __name__ == "__main__":
 
     tabla = [4,2,6,3,8,7,5,9,1,0]
     print (tabla)
     #Definimos nuestra unica instancia de clase
-    resultado = Dicotonomia(tabla)
+    resultado = Dicotonomia_1(tabla)
     print ("La tabla ordenada por dicotonomia es: {}".format(resultado.ordenar()))
