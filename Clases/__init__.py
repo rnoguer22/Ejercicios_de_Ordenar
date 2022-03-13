c74a1482f055a6a6f7eb89d9a859f360ba14ccb4
rnoguer22 <rnogugon@myuax.com>
2022-03-13 10:51:24 +0100

    Solucion error en __init__.py

--- Clases/__init__.py
+++ Clases/__init__.py
@@ -1,13 +1,18 @@
-from .Insercion_dicotonomica import (
-    ordenar, 
-    ordenar_lista_vacia
+try:  
+    from Clases.Insercion_dicotonomica import (
+        ordenar, 
+        ordenar_lista_vacia
+        )
+
+    from Clases.Ordenacion_topologica import (
+        ordenacion_burbuja
     )
 
-from .Ordenacion_topologica import (
-    ordenacion_burbuja
-)
-
-from .Explorar import (
-    segmentos,
-    explorar
-)
+    from Clases.Explorar import (
+        segmentos,
+        explorar
+    )
+except ImportError:
+    #Como no usamos estas clases, podemos saltarnoslas mediante una exepcion para que compile
+    print ("ImportError inesperado en __init__.py")
+    pass
